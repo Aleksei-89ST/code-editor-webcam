b3465fc484726eba68959fc61e3641299687286b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-20 22:39:05 +0400

    настраиваю табы

---
+++ src/components/CodeBar/CodeBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import styles from "./CodeBar.module.css";
+
+const CodeBar = () => {
+  const [activeTab, setActiveTab] = useState("html");
+  return (
+    <div className={styles.codebar}>
+      <nav className={styles.tab}>
+        <button
+          onClick={() => {
+            setActiveTab("html");
+          }}
+          className={`${styles.item} ${
+            activeTab === "html" ? styles.activeTab : ""
+          }`}
+        >
+          HTML
+        </button>
+        <button
+          onClick={() => {
+            setActiveTab("css");
+          }}
+          className={`${styles.item} ${
+            activeTab === "css" ? styles.activeTab : ""
+          }`}
+        >
+          CSS
+        </button>
+        <button
+          onClick={() => {
+            setActiveTab("js");
+          }}
+          className={`${styles.item} ${
+            activeTab === "js" ? styles.activeTab : ""
+          }`}
+        >
+          JS
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default CodeBar;
