f1056f4829d4004b6ac5d4f7cbeadab539b252d7
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-21 13:37:05 +0400

    будет хранить наше состояние

--- src/components/Editor/HtmlEditor.js
+++ src/components/Editor/HtmlEditor.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/snippets/html";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { EditorContext } from '../../context/context';
 
 const HtmlEditor = () => {
+  // таким образом получаем доступ к HTML
+  const {html ,setHtml} = useContext(EditorContext)
   return (
     <AceEditor
     placeholder="Write your HTML codes here!"
     mode="html"
     theme="monokai"
     name="editor_html"
+    value={html}
+    onChange={value => setHtml(value)}
     fontSize="16"
     heigth={"100%"}
     width={"100%"}
--- src/components/Editor/JsEditor.js
+++ src/components/Editor/JsEditor.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/snippets/javascript";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { EditorContext } from "../../context/context";
 
 function JsEditor() {
+  const {js,setJs} = useContext(EditorContext)
   return (
     <AceEditor
       placeholder="Write your JS codes here!"
       mode="javascript"
       theme="monokai"
       name="editor_js"
+      value={js}
+      onChange={value => setJs(value)}
       fontSize="16"
       heigth={"100%"}
       width={"100%"}
